aec7bcea40947edf5d2862f76e19bcfd55068ce2
Marta1706 <marta.kar.zuber@gmail.com>
2021-01-22 16:16:32 +0100

    js

---
+++ main.js
@@ -0,0 +1,33 @@
+// es 5
+var imiona_prac = "Róża"
+// es 6
+
+let imiona_prac3 = "kamil"
+
+const numString = "10"
+const num = 10
+const num0 = 1
+const num1 = 1
+const prawda = true // to 1 jako liczba
+const fałsz = false // to zero jako liczba
+
+console.log(num0 >= num1);
+console.log(num1 + num0);
+console.log(num0 - num1);
+console.log(num0 !== num1)
+
+const arr = ["Janusz", 55, true, num]
+console.log(arr[2])
+
+const obj = {
+    name: "janusz",
+    age: 13,
+    adress: "nowa 15"
+}
+console.log(obj.age)
+
+const respons = [
+    { name: "Dawid" },
+    { name: "Jakub" }
+]
+console.log(respons[1])
